5fc013709c410fddfe8f85d6d50e6e8420cd17ee
unknown <rostik057@gmail.com>
2024-04-06 13:09:29 +0300

    !fix chat

--- src/app/store/utils.js
+++ src/app/store/utils.js
@@ -1,2 +1,2 @@
-export const URL_BACKEND = "https://other.punter.website/api";
+export const URL_BACKEND = "https://other.punter.website/api/";
 // export const URL_BACKEND = "http://127.0.0.1:8000/api/";
